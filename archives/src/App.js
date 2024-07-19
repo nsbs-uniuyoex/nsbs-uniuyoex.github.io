071987d9146004dba020e9b7078da1f15010d1b4
Abdiel Wilson <wilsonabdiel86@gmail.com>
2024-07-19 17:28:08 +0100

    Update App.js

--- archives/src/App.js
+++ archives/src/App.js
@@ -1,8 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Nav from "./components/Nav";
 import Header from "./components/Header";
-import Levels from "./components/Levels";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 function App() {
